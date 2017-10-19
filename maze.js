dc988146e41fc10c8d24719324ed77e2f8895501
Yanzzz27 <32621396+Yanzzz27@users.noreply.github.com>
2017-10-19 16:35:37 -0500

    Excercise2

--- maze.js
+++ maze.js
@@ -1,6 +1,16 @@
 window.onload= function(){
 	var boundary=document.getElementById("boundary1")
+	var allboundary= document.querySelectorAll(".boundary");
+
 	boundary.onmouseover=function () {
 		boundary.classList.add("youlose");
 	}
+	for (var i = 0; i < allboundary.length-1; i++) {
+		allboundary[i].onmouseover = function (){
+			for (var i = 0; i < allboundary.length-1; i++) {
+				allboundary[i].classList.add("youlose");
+			}
+		}
+	}
+
 }
